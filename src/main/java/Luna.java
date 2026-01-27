d01808f7ebf680b8c23a7f0db18940a3773b65d3
Sarina Ke <sarinake@SarinaMacbookAir.local>
2026-01-28 04:11:15 +0800

    Implement skeletal ver of chatbot

---
+++ src/main/java/Luna.java
@@ -0,0 +1,12 @@
+public class Luna {
+    public static void main(String[] args) {
+        final String LINE = "____________________________________________________________\n";
+
+        System.out.println(LINE);
+        System.out.println("Hello! I'm Luna\n");
+        System.out.println("What can I do for you?\n");
+        System.out.println(LINE);
+        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println(LINE);
+    }
+}
