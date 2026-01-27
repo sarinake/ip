a1e6f366b63c60a207132919de54ee74bf676243
Sarina Ke <sarinake@SarinaMacbookAir.local>
2026-01-28 04:35:43 +0800

    Add ability to store user input and list them back when requested

--- src/main/java/Luna.java
+++ src/main/java/Luna.java
@@ -2,19 +2,31 @@ import java.util.Scanner;
 
 public class Luna {
     public static void main(String[] args) {
+        String[] tasks = new String[100];
+        int count = 0;
+
         Scanner sc = new Scanner(System.in);
         final String LINE = "____________________________________________________________\n";
 
-        System.out.println("Hello! I'm Luna\n");
-        System.out.println("What can I do for you?\n");
+        System.out.println("Hello! I'm Luna");
+        System.out.println("What can I do for you?");
 
         while (true) {
             String input = sc.nextLine();
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < count; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(LINE);
+                continue;
             }
-            System.out.println(input);
+
+            tasks[count] = input;
+            count++;
+            System.out.println("added: " + input);
             System.out.println(LINE);
         }
         sc.close();
