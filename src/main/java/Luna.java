92bea92601b4b280534e917d92e08529be7961c4
Sarina Ke <sarinake@SarinaMacbookAir.local>
2026-01-28 04:19:36 +0800

    Improve skeletal ver to echo user commands and exit

--- src/main/java/Luna.java
+++ src/main/java/Luna.java
@@ -1,12 +1,22 @@
+import java.util.Scanner;
+
 public class Luna {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         final String LINE = "____________________________________________________________\n";
 
-        System.out.println(LINE);
         System.out.println("Hello! I'm Luna\n");
         System.out.println("What can I do for you?\n");
-        System.out.println(LINE);
-        System.out.println("Bye. Hope to see you again soon!\n");
-        System.out.println(LINE);
+
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(input);
+            System.out.println(LINE);
+        }
+        sc.close();
     }
 }
