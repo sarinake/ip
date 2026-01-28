1408ea92e9244f416766e81a5da9f12262e0c9ca
Sarina Ke <sarinake@SarinaMacbookAir.local>
2026-01-29 00:16:48 +0800

    add support for tracking 3 types of tasks

--- src/main/java/Luna.java
+++ src/main/java/Luna.java
@@ -1,13 +1,17 @@
 import java.util.Scanner;
 
 public class Luna {
+    private Task[] tasks = new Task[100];
+    private int count = 0;
+
+    private static final String LINE = "____________________________________________________________\n";
+
     public static void main(String[] args) {
+        new Luna().run();
+    }
+
+    public void run() {
         Scanner sc = new Scanner(System.in);
-
-        Task[] tasks = new Task[100];
-        int count = 0;
-
-        final String LINE = "____________________________________________________________\n";
 
         System.out.println("Hello! I'm Luna");
         System.out.println("What can I do for you?");
@@ -15,33 +19,54 @@ public class Luna {
         while (true) {
             String input = sc.nextLine();
 
-            if (input.equals("bye")) {
+            if (input.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else if (input.equals("list")) {
+            } else if (input.equalsIgnoreCase("list")) {
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < count; i++) {
                     System.out.println((i + 1) + ". " + tasks[i]);
                 }
             } else if (input.startsWith("mark")) {
-                int index = Integer.parseInt(input.substring(5)) - 1;
+                int index = Integer.parseInt(input.substring("mark ".length()).trim()) - 1;
                 tasks[index].markDone();
                 System.out.println("Nice! I've marked this task as done:");
                 System.out.println(tasks[index]);
             } else if (input.startsWith("unmark")) {
-                int index = Integer.parseInt(input.substring(7)) - 1;
+                int index = Integer.parseInt(input.substring("unmark ".length()).trim()) - 1;
                 tasks[index].markUndone();
                 System.out.println("OK, I've marked this task as not done yet:");
                 System.out.println(tasks[index]);
-            } else {
-                tasks[count] = new Task(input);
-                count++;
-                System.out.println("added: " + input);
+            } else if (input.startsWith("todo")) {
+                String desc = input.substring("todo ".length()).trim();
+                addTask(new Todo(desc));
+            } else if (input.startsWith("deadline")) {
+                String[] arr = input.substring("deadline ".length()).trim().split(" /by ");
+                String desc = arr[0];
+                String deadline = arr[1];
+                addTask(new Deadline(desc, deadline));
+            } else if (input.startsWith("event")) {
+                String rest = input.substring("event ".length()).trim();
+                int fromIndex = rest.indexOf(" /from ");
+                int toIndex = rest.indexOf(" /to ");
+
+                String desc = rest.substring(0, fromIndex);
+                String startDate = rest.substring(fromIndex + " /from ".length(), toIndex);
+                String endDate = rest.substring(toIndex + " /to ".length());
+                addTask(new Event(desc, startDate, endDate));
             }
 
             System.out.println(LINE);
         }
 
         sc.close();
+    }
+
+    public void addTask(Task task) {
+        tasks[count] = task;
+        count++;
+        System.out.println("Got it. I've added this task:");
+        System.out.println(tasks[count - 1]);
+        System.out.println("Now you have " + count + " tasks in the list.");
     }
 }
