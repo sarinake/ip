a1e746de89823c89c913411796f1e0e14811ecb5
Sarina Ke <sarina.y.ke@gmail.com>
2026-08-21 14:25:06 +0800

    Add ability to mark tasks as done/undone

--- src/main/java/Luna.java
+++ src/main/java/Luna.java
@@ -2,10 +2,11 @@ import java.util.Scanner;
 
 public class Luna {
     public static void main(String[] args) {
-        String[] tasks = new String[100];
+        Scanner sc = new Scanner(System.in);
+
+        Task[] tasks = new Task[100];
         int count = 0;
 
-        Scanner sc = new Scanner(System.in);
         final String LINE = "____________________________________________________________\n";
 
         System.out.println("Hello! I'm Luna");
@@ -13,22 +14,34 @@ public class Luna {
 
         while (true) {
             String input = sc.nextLine();
+
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             } else if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < count; i++) {
                     System.out.println((i + 1) + ". " + tasks[i]);
                 }
-                System.out.println(LINE);
-                continue;
+            } else if (input.startsWith("mark")) {
+                int index = Integer.parseInt(input.substring(5)) - 1;
+                tasks[index].markDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(tasks[index]);
+            } else if (input.startsWith("unmark")) {
+                int index = Integer.parseInt(input.substring(7)) - 1;
+                tasks[index].markUndone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(tasks[index]);
+            } else {
+                tasks[count] = new Task(input);
+                count++;
+                System.out.println("added: " + input);
             }
 
-            tasks[count] = input;
-            count++;
-            System.out.println("added: " + input);
             System.out.println(LINE);
         }
+
         sc.close();
     }
 }
