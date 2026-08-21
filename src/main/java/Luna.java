30657165e2b5791578aee64a9d2c741009152eef
Sarina Ke <sarina.y.ke@gmail.com>
2026-08-21 14:18:14 +0800

    Update code to echo user commands, exit when user types bye

--- src/main/java/Luna.java
+++ src/main/java/Luna.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Luna {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         final String LINE = "____________________________________________________________\n";
 
         System.out.println(LINE);
@@ -8,5 +11,16 @@ public class Luna {
         System.out.println(LINE);
         System.out.println("Bye. Hope to see you again soon!\n");
         System.out.println(LINE);
+
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(input);
+            System.out.println(LINE);
+        }
+        sc.close();
     }
 }
